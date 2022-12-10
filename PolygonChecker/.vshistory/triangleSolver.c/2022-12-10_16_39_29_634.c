8fd565040276e82835a38876d47575b5fc5af4d4
Dominic <Dszymanski7442@conestogac.on.ca>
2022-12-10 18:32:16 -0500

    added some notes

--- PolygonChecker/.vshistory/triangleSolver.c/2022-12-10_16_39_29_634.c
+++ PolygonChecker/.vshistory/triangleSolver.c/2022-12-10_16_39_29_634.c
@@ -2,14 +2,6 @@
 #include <stdbool.h>
 #include <math.h>
 #include "triangleSolver.h"
-/*Dominic Szymanski - Quinton Jackson - kaiden Providence - CSCN7020 - Group 24 - 2022/12/01
-Given the Program we implemented a get angle function that will solve for three angles within a trianlge given all three
-sides using sin law cosine law. We then implemented the recatnglefunctions. The first function is for user inputs and takes 4 xy coordiantes as input
-The next function will determine the length of the lines but subtracting the and x/y values by each other then using Pythagorean theorem.
-Those values are then passed to the next which function which to determine whether or not it is a rectangle or square if a square or rectangle
-it will provide the user with the perimeter and area.*/
-
-
 
 char* analyzeTriangle(int side1, int side2, int side3)
 {
---
+++ Rectangle Perim and Area Testing/Rectangle Perim and Area Testing.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RectanglePerimandAreaTesting
+{
+	TEST_CLASS(RectanglePerimandAreaTesting)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
