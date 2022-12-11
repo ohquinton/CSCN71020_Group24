771c3e19b28476882261699b92b583aecd8c57e1
ohquinton <ohquinton01@gmail.com>
2022-12-10 21:31:38 -0500

    re-added test case after encountering bug

--- Rectangle Perim and Area Testing/Rectangle Perim and Area Testing.cpp
+++ Rectangle Perim and Area Testing/Rectangle Perim and Area Testing.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+extern "C" int calculate_rectangle_area(int length1, int length2);
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace RectanglePerimandAreaTesting
@@ -8,9 +9,16 @@ namespace RectanglePerimandAreaTesting
 	TEST_CLASS(RectanglePerimandAreaTesting)
 	{
 	public:
-		
+
 		TEST_METHOD(TestMethod1)
 		{
+			int length1 = 2;
+			int length2 = 3;
+			int expectedArea = 6;
+
+			int actualArea = calculate_rectangle_area(length1, length2);
+
+			Assert::AreEqual(expectedArea, actualArea);
 		}
 	};
 }
